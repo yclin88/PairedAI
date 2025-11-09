fa6ab9acc69dbb886e6ccf300a34efad6caa01b6
Yih-Chuan Lin <88579795+yclin88@users.noreply.github.com>
2025-11-09 09:37:15 +0800

    modifying main.cpp for displaying two windows horizontally

--- main.cpp
+++ main.cpp
@@ -5,7 +5,12 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Widget w;
-    w.show();
+    Widget w1,w2;
+    w1.resize(300,200);
+    w2.resize(300,200);
+    w1.move(100,100);
+    w2.move(450,100);
+    w1.show();
+    w2.show();
     return a.exec();
 }
