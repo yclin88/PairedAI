a7fb621878e870524bd591df3830f0a40d69c961
yclin88 <lyc@nfu.edu.tw>
2025-11-09 09:49:23 +0800

    modifying ui to add one button by S2 at prj/ui

--- ui_widget.h
+++ ui_widget.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'widget.ui'
 **
-** Created by: Qt User Interface Compiler version 6.9.2
+** Created by: Qt User Interface Compiler version 6.1.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -12,6 +12,7 @@
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QGroupBox>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QRadioButton>
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
@@ -25,27 +26,63 @@ public:
     QVBoxLayout *verticalLayout;
     QRadioButton *radioButton;
     QRadioButton *radioButton_2;
+    QPushButton *pushButton;
 
     void setupUi(QWidget *Widget)
     {
         if (Widget->objectName().isEmpty())
-            Widget->setObjectName("Widget");
+            Widget->setObjectName(QString::fromUtf8("Widget"));
         Widget->resize(800, 600);
         groupBox = new QGroupBox(Widget);
-        groupBox->setObjectName("groupBox");
+        groupBox->setObjectName(QString::fromUtf8("groupBox"));
         groupBox->setGeometry(QRect(80, 60, 114, 79));
+        QPalette palette;
+        QBrush brush(QColor(170, 0, 0, 255));
+        brush.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::WindowText, brush);
+        QBrush brush1(QColor(80, 101, 240, 255));
+        brush1.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::Button, brush1);
+        QBrush brush2(QColor(255, 85, 0, 255));
+        brush2.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::ButtonText, brush2);
+        QBrush brush3(QColor(255, 255, 121, 255));
+        brush3.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::Base, brush3);
+        palette.setBrush(QPalette::Inactive, QPalette::WindowText, brush);
+        palette.setBrush(QPalette::Inactive, QPalette::Button, brush1);
+        palette.setBrush(QPalette::Inactive, QPalette::ButtonText, brush2);
+        palette.setBrush(QPalette::Inactive, QPalette::Base, brush3);
+        QBrush brush4(QColor(120, 120, 120, 255));
+        brush4.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Disabled, QPalette::WindowText, brush4);
+        palette.setBrush(QPalette::Disabled, QPalette::Button, brush1);
+        palette.setBrush(QPalette::Disabled, QPalette::ButtonText, brush4);
+        QBrush brush5(QColor(240, 240, 240, 255));
+        brush5.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Disabled, QPalette::Base, brush5);
+        groupBox->setPalette(palette);
+        QFont font;
+        font.setFamilies({QString::fromUtf8("Agency FB")});
+        font.setPointSize(12);
+        groupBox->setFont(font);
+        groupBox->setCheckable(true);
+        groupBox->setChecked(false);
         verticalLayout = new QVBoxLayout(groupBox);
-        verticalLayout->setObjectName("verticalLayout");
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
         radioButton = new QRadioButton(groupBox);
-        radioButton->setObjectName("radioButton");
+        radioButton->setObjectName(QString::fromUtf8("radioButton"));
 
         verticalLayout->addWidget(radioButton);
 
         radioButton_2 = new QRadioButton(groupBox);
-        radioButton_2->setObjectName("radioButton_2");
+        radioButton_2->setObjectName(QString::fromUtf8("radioButton_2"));
 
         verticalLayout->addWidget(radioButton_2);
 
+        pushButton = new QPushButton(Widget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(90, 160, 75, 23));
 
         retranslateUi(Widget);
 
@@ -58,6 +95,7 @@ public:
         groupBox->setTitle(QCoreApplication::translate("Widget", "\346\200\247\345\210\245", nullptr));
         radioButton->setText(QCoreApplication::translate("Widget", "\347\224\267\347\224\237", nullptr));
         radioButton_2->setText(QCoreApplication::translate("Widget", "\345\245\263\347\224\237", nullptr));
+        pushButton->setText(QCoreApplication::translate("Widget", "\347\242\272\345\256\232", nullptr));
     } // retranslateUi
 
 };
